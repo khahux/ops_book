380b3cc4a1bbaccf6ef4d6b2df4e91627c1a68e5
khahux <khahux@gmail.com>
2023-05-19 17:24:38 +0800

    install git

--- linux_deploy/softwares/git/install_git-2.40.1.sh
+++ linux_deploy/softwares/git/install_git-2.40.1.sh
@@ -3,6 +3,7 @@
 version="2.40.1"
 
 # yum install curl-devel expat-devel gettext-devel openssl-devel zlib-devel gcc perl-ExtUtils-MakeMaker
+# apt install make libghc-zlib-dev libexpat1-dev libssl-dev libcurl4-gnutls-dev gettext unzip libcurl4-openssl-dev -y
 
 if [ ! -f v${version}.tar.gz ];then
     wget https://github.com/git/git/archive/v${version}.tar.gz
